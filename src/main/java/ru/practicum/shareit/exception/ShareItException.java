8f9f75752acfdd6f2e952e2a372dd5b8835b557a
Александр Германов <mailmate@yandex.ru>
2023-10-31 05:37:18 +0300

    удалить UserServiceImpl

--- src/main/java/ru/practicum/shareit/exception/ShareItException.java
+++ src/main/java/ru/practicum/shareit/exception/ShareItException.java
@@ -5,12 +5,4 @@ public class ShareItException extends RuntimeException {
     public ShareItException(String message) {
         super(message);
     }
-
-    public ShareItException(Throwable cause) {
-        super(cause);
-    }
-
-    public ShareItException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
