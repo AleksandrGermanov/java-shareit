2fa8d483e6823641034f21790d6cc54f89529eae
Александр Германов <mailmate@yandex.ru>
2023-10-31 04:23:20 +0300

    снова создать папку util

---
+++ src/main/java/ru/practicum/shareit/util/ShareItValidator.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class ShareItValidator {
+    private final Validator validator;
+
+    public <T> void validate(T t) {
+        Set<ConstraintViolation<T>> violations = validator.validate(t);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
